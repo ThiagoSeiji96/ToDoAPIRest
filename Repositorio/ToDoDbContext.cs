09e9491afc8e00dcd452cf510de580ea633a166b
Thiago Rozner <thiagorozner@gmail.com>
2023-05-03 12:13:54 -0300

    Chore: Conexao com DB

--- Repositorio/ToDoDbContext.cs
+++ Repositorio/ToDoDbContext.cs
@@ -6,17 +6,6 @@ namespace Repositorio
 {
     public class ToDoDbContext : DbContext
     {
-        //public ToDoDbContext()
-        //{
-        //    Tarefas = new List<Tarefa>
-        //    {
-        //        new Tarefa(1, "Tarefa 1", "Descrição 1"),
-        //        new Tarefa(2, "Tarefa 2", "Descrição 2"),
-        //    };
-        //}
-
-
-        //CONSTRUTOR PARA CONEXÃO COM BANCO DE DADOS
         public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
         {
 
